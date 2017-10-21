18d868a088b2ab3764cdc0d58cfe21b18cb597bb
Alex Craiu <acraiu.dev@gmail.com>
2017-10-21 13:08:02 +0200

    fix nav title duplicates

--- src/nav-title.component.ts
+++ src/nav-title.component.ts
@@ -4,9 +4,8 @@ import {
   ActivatedRoute,
   ActivatedRouteSnapshot,
   NavigationEnd,
-  Params,
-  Event as RouterEvent,
-  PRIMARY_OUTLET } from '@angular/router';
+  PRIMARY_OUTLET
+} from '@angular/router';
 import 'rxjs/add/operator/filter';
 
 // default 
@@ -20,10 +19,10 @@ export class NavTitle {
 @Component({
   selector: 'nav-title',
   template:
-    `<span *ngFor="let t of titles; let last = last;">
-      <a routerLink="t.url">{{t.label}}</a><span *ngIf="!last"> {{separator}}</span>
-    </span>`,
-  styles: ['a{text-decoration:none;}']
+  `<span *ngFor="let t of titles; let last = last;">
+    <a routerLink="t.url">{{t.label}}</a><span *ngIf="!last"> {{separator}}</span>
+  </span>`,
+styles: ['a{text-decoration:none;}']
 })
 export class NavTitleComponent implements OnInit {
   @Input() separator: string = '–';
@@ -51,12 +50,17 @@ export class NavTitleComponent implements OnInit {
     return this.getLastChildRoute(children[0])
   }
 
+  // checks if the data.navTitle of the route is actually propagated from the parent route
+  private isPropagatedData(childRoute: ActivatedRouteSnapshot, parentRoute: ActivatedRouteSnapshot) {
+    return parentRoute && parentRoute.data[this.dataParam] === childRoute.data[this.dataParam];
+  }
+
   private getNavTitles(activatedSnapshot: ActivatedRouteSnapshot): NavTitle[] {
     let pathFromRoute = this.getLastChildRoute(activatedSnapshot).pathFromRoot;
     let titles: NavTitle[] = [];
-    
+
     for (let route of pathFromRoute) {
-      if (route.data.hasOwnProperty(this.dataParam) && typeof route.data[this.dataParam] === 'string') {
+      if (route.data.hasOwnProperty(this.dataParam) && typeof route.data[this.dataParam] === 'string' && !this.isPropagatedData(route, route.parent)) {
         titles.push({
           label: route.data[this.dataParam],
           url: route.pathFromRoot.filter(r => r.url.length > 0).map(r => r.url[0]).join('/')
